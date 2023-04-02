f16d6b77366c849aa819c6d286e488c5e8180b82
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-04-02 22:56:29 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[] solution(int num, int total) {
         int[] arr = new int[num];
-        
+        int i = 0 ;
+        int j = 0 ;
         if(total%num == 0){
-            int i = 0 ;
-            int j = 0 ;
             i = ((total/num) - (num/2));
             for(int k = i; k <(i+num); k++){
                 arr[j++] = k;
             }
         } else {
-            int i = 0 ;
-            int j = 0 ;
             i = ((total/num) - (num/2 -1));
             for(int k = i; k <(i+num); k++){
                 arr[j++] = k;
