1d986d70a40fc7a40a7c1a972048a2d24b79088c
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-04-08 12:26:28 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double result = 0;
+        int sum =0;
+        
+        for(int i=0; i<numbers.length; i++){
+            sum += numbers[i];
+        }
+        result = (double) sum/numbers.length;
+        return result;
+    }
+}
