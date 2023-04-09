3e4ede49ccdf4c6a3a19f00a4f31aad69c23a21c
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-04-09 14:51:57 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.05 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int result = 1;
+            if(num1 != num2){
+                 result = -1;
+            }  
+        return result;
+    }
+}
