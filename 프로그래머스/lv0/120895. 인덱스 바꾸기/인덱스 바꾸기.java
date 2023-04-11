d23a87540595a301e3ab4c9b7fd4bed42230f2ae
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-04-11 12:21:55 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 9.82 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,16 +1,15 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         String result = "";
-        for(int i=0; i<my_string.length(); i++){
-            char str = my_string.charAt(i);
-            if(i == num1) {
-                str = my_string.charAt(num2);
-            } else if(i == num2){
-                str = my_string.charAt(num1);
-            }
-            result += str;
-        }
         
+        char[] ch = my_string.toCharArray();
+        char tmp = ch[num1];
+        ch[num1] = ch[num2];
+        ch[num2] =tmp;
+
+        for(char word : ch){
+            result += word;
+        }
         return result;
     }
 }
