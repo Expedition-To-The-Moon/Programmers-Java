985807a41635a526feb6302bf5580920889f7771
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-06-07 22:13:59 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.33 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] res = new int[5];
+        Arrays.sort(num_list);
+        for(int i=0; i<5; i++){
+            res[i] = num_list[i];
+        }
+        return res;
+    }
+}
