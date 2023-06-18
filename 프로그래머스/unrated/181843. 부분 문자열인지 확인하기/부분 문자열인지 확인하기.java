c7a3bcd657f7f2cc4b7afa48ebf19607b3e0c228
Expedition-To-The-Moon <128493900+Expedition-To-The-Moon@users.noreply.github.com>
2023-06-18 12:09:07 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int res = 0;
+        if(my_string.contains(target)){
+            res = 1;
+        }
+        return res;
+    }
+}
